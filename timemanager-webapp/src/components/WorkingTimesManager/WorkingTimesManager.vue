b88ce1331ed68ce9e9d60778360be3b6a13d6c4c
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-10-15 11:44:16 +0200

    changed workingTimesManager.vue in vue3 syntax

--- timemanager-webapp/src/components/WorkingTimesManager/WorkingTimesManager.vue
+++ timemanager-webapp/src/components/WorkingTimesManager/WorkingTimesManager.vue
@@ -1,57 +1,56 @@
-<script>
+<script setup>
 import axios from 'axios'
+import { useRoute } from 'vue-router'
+import { ref, onMounted, watch } from 'vue'
 
-export default {
-  data() {
-    return {
-      userId: this.$route.params.userId,
-      workingTimes: []
-    }
-  },
-  methods: {
-    async getWorkingTimes() {
-      if (this.userId) {
-        try {
-          const response = await axios.get(`http://localhost:4000/api/workingtime/${this.userId}`)
-          this.workingTimes = response.data
-        } catch (error) {
-          console.error('Error fetching working times:', error)
-        }
-      }
-    },
-    formatDate(dateString) {
-      return new Date(dateString).toLocaleString()
-    }
-  },
-  mounted() {
-    this.getWorkingTimes()
-  },
-  watch: {
-    '$route.params.userId': function(newUserId) {
-      this.userId = newUserId
-      this.getWorkingTimes()
+const route = useRoute()
+
+const userId = ref(route.params.userId)
+const workingTimes = ref([])
+
+async function getWorkingTimes() {
+  if (userId.value) {
+    try {
+      const response = await axios.get(`http://localhost:4000/api/workingtime/${userId.value}`)
+      workingTimes.value = response.data
+    } catch (error) {
+      console.error('Error fetching working times:', error)
     }
   }
 }
+
+function formatDate(dateString) {
+  return new Date(dateString).toLocaleString()
+}
+
+onMounted(async () => {
+  getWorkingTimes()
+})
+watch(
+  () => route.params.userId,
+  (newUserId) => {
+    userId.value = newUserId
+    getWorkingTimes()
+  }
+)
 </script>
 <template>
-    <div>
-      <h2>Working Times</h2>
-      <table v-if="workingTimes.length">
-        <thead>
-          <tr>
-            <th>Start</th>
-            <th>End</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="workingTime in workingTimes" :key="workingTime.id">
-            <td>{{ formatDate(workingTime.start) }}</td>
-            <td>{{ formatDate(workingTime.end) }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <p v-else>No working times found for this user.</p>
-    </div>
-  </template>
-  
+  <div>
+    <h2>Working Times</h2>
+    <table v-if="workingTimes.length">
+      <thead>
+        <tr>
+          <th>Start</th>
+          <th>End</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="workingTime in workingTimes.value" :key="workingTime.id">
+          <td>{{ formatDate(workingTime.start) }}</td>
+          <td>{{ formatDate(workingTime.end) }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>No working times found for this user.</p>
+  </div>
+</template>
