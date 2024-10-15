59451f47c186e01b4df02471350bc44a73041153
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-10-15 10:11:49 +0200

    changed App.vue

--- timemanager-webapp/src/App.vue
+++ timemanager-webapp/src/App.vue
@@ -7,19 +7,13 @@
       <router-link :to="'/chartManager/' + userId">Charts</router-link> |
       <router-link :to="'/workingtimes/' + userId">Working Times</router-link> |
       <routerLink to="/workingTime">Working Time</routerLink> |
-        
+
     </nav>
     <router-view></router-view>
   </div>
-  
+
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      userId: 1 // Replace with actual user ID
-    }
-  }
-}
+<script setup>
+const userId = 1
 </script>
