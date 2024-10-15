f3f00ed00ec1455dd95d0bbbf45925447401ed46
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-10-15 11:31:31 +0200

    changed workingtimemanager into vue3 syntax

--- timemanager-webapp/src/components/WorkingTimesManager/WorkingTimeManager.vue
+++ timemanager-webapp/src/components/WorkingTimesManager/WorkingTimeManager.vue
@@ -1,35 +1,31 @@
+<script setup>
+import axios from 'axios'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
 
-  <script>
-  import axios from 'axios'
-  
-  export default {
-    data() {
-      return {
-        userId: this.$route.params.userId,
-        workingTimeId: this.$route.params.workingtimeId,
-        workingTime: { start: '', end: '' }
-      }
-    },
-    methods: {
-      async createWorkingTime() {
-        await axios.post(`http://localhost:4000/api/workingtime/${this.userId}`, this.workingTime)
-      },
-      async updateWorkingTime() {
-        await axios.put(`http://localhost:4000/api/workingtime/${this.workingTimeId}`, this.workingTime)
-      },
-      async deleteWorkingTime() {
-        await axios.delete(`http://localhost:4000/api/workingtime/${this.workingTimeId}`)
-      }
-    }
-  }
-  </script>
-  <template>
-    <div>
-      <h2>Working Time</h2>
-      <input v-model="workingTime.start" type="datetime-local">
-      <input v-model="workingTime.end" type="datetime-local">
-      <button @click="createWorkingTime">Create</button>
-      <button @click="updateWorkingTime">Update</button>
-      <button @click="deleteWorkingTime">Delete</button>
-    </div>
-  </template>
+const route = useRoute()
+
+const userId = ref(route.params.userId)
+const workingTimeId = ref(route.params.workingtimeId)
+const workingTime = ref({ start: '', end: '' })
+
+async function createWorkingTime() {
+  await axios.post(`http://localhost:4000/api/workingtime/${userId.value}`, workingTime)
+}
+async function updateWorkingTime() {
+  await axios.put(`http://localhost:4000/api/workingtime/${workingTimeId.value}`, workingTime)
+}
+async function deleteWorkingTime() {
+  await axios.delete(`http://localhost:4000/api/workingtime/${workingTimeId.value}`)
+}
+</script>
+<template>
+  <div>
+    <h2>Working Time</h2>
+    <input v-model="workingTime.start" type="datetime-local" />
+    <input v-model="workingTime.end" type="datetime-local" />
+    <button @click="createWorkingTime">Create</button>
+    <button @click="updateWorkingTime">Update</button>
+    <button @click="deleteWorkingTime">Delete</button>
+  </div>
+</template>
