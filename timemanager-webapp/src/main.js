38a4bf1390c0c78422fa26c954ae984208087cbb
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-10-21 17:30:58 +0200

    added vuestic dependecies

--- timemanager-webapp/src/main.js
+++ timemanager-webapp/src/main.js
@@ -8,7 +8,6 @@ import router from './router'
 
 import { createVuestic } from "vuestic-ui";
 import 'vuestic-ui/css'; // Default Vuestic UI styles
-import 'material-design-icons-iconfont/dist/material-design-icons.min.css'; // Material Design Icons
 
 const app = createApp(App)
 
