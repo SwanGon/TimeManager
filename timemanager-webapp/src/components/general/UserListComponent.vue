a5aa2f0003047028fbcf9260c4ded7554ca52212
LaurentLF2023 <laurent.le-foulgoc@epitech.eu>
2024-10-24 17:11:03 +0200

    add UserListComponent.vue and MyTeamView.vue

---
+++ timemanager-webapp/src/components/general/UserListComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <table class="w-2/5 my-4">
+        <thead class="border-black bg-gray-100 border border-collapse">
+            <th class="p-3 border-r">Username</th>
+            <th class="p-3">Email</th>
+        </thead>
+        <tbody>
+            <tr :key="user.id" v-for="user in userlist"
+                class="border-black bg-gray-100 border border-collapse"
+            >
+                <td class="p-3 text-center border-r border-black">{{ user.username }}</td>
+                <td class="p-3 text-center">{{ user.email }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const {userlist} = defineProps([
+  "userlist"
+])
+</script>
