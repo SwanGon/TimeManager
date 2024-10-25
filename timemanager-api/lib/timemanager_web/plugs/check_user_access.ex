789ee7171aea506890c3b55711191783920be528
Rolland-tsokeng <89741988+Rolland-tsokeng@users.noreply.github.com>
2024-10-25 16:33:30 +0200

    style: 🎨 add more design on th readme

--- timemanager-api/lib/timemanager_web/plugs/check_user_access.ex
+++ timemanager-api/lib/timemanager_web/plugs/check_user_access.ex
@@ -6,24 +6,15 @@ defmodule TimemanagerWeb.Plug.CheckUserAccess do
 
   def call(conn, _opts) do
     user_id = conn.assigns[:current_user].id
-    params_user_id = conn.params["user_id"]
+    params_user_id = String.to_integer(conn.params["user_id"])
 
-    if params_user_id == nil do
+    if user_id != params_user_id do
       conn
-      |> put_status(:bad_request)
-      |> json(%{error: "Missing user_id parameter"})
+      |> put_status(:unauthorized)
+      |> json(%{error: "Unauthorized"})
       |> halt()
     else
-      params_user_id = String.to_integer(params_user_id)
-
-      if user_id != params_user_id do
-        conn
-        |> put_status(:unauthorized)
-        |> json(%{error: "Unauthorized"})
-        |> halt()
-      else
-        conn
-      end
+      conn
     end
   end
 end
