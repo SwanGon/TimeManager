e64b1ec9b7d044102456b440a42a02e0f1572df7
LaurentLF2023 <laurent.le-foulgoc@epitech.eu>
2024-10-25 11:44:18 +0200

    FRONT: fix router imports

--- timemanager-webapp/src/router/index.js
+++ timemanager-webapp/src/router/index.js
@@ -2,22 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ClockManager from '../components/ClockManager/ClockManager.vue'
 import ChartManager from '../components/ChartManager/ChartManager.vue'
-<<<<<<< HEAD
 // import UserManager from '@/components/UserManager/UserManager'
 import WorkingTimesManager from '@/components/WorkingTimesManager/WorkingTimesManager.vue'
 import WorkingShiftManager from '@/components/WorkingShiftManager/WorkingShiftManager.vue'
 import MyTeamView from '@/views/MyTeamView.vue'
-
-=======
 import TeamManager from'@/components/UserManager/TeamManager.vue'
-import UserManager from '@/components/UserManager/UserManager.vue'
-import WorkingTimesManager from '@/components/WorkingTimesManager/WorkingTimesManager.vue'
-import WorkingShiftManager from '@/components/WorkingShiftManager/WorkingShiftManager.vue'
 import LoginView from '@/views/Authentication/LoginView.vue'
 import RegisterView from '@/views/Authentication/RegisterView.vue'
 import ProfilView from '@/views/Authentication/ProfilView.vue'
 import Contracts from '@/views/Contracts.vue'
->>>>>>> init
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
