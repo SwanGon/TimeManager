1a538e57fbbf4bfdcc322b8fb263c7c64b14ed4b
LaurentLF2023 <laurent.le-foulgoc@epitech.eu>
2024-10-27 13:32:41 +0100

    :fix text color

--- timemanager-webapp/src/components/general/UserListComponent.vue
+++ timemanager-webapp/src/components/general/UserListComponent.vue
@@ -1,15 +1,15 @@
 <template>
     <table class="w-2/5 my-4">
         <thead class="border-black bg-gray-100 border border-collapse">
-            <th class="p-3 border-r">Username</th>
-            <th class="p-3">Email</th>
+            <th class="p-3 text-gray-700 border-r">Username</th>
+            <th class="p-3 text-gray-700">Email</th>
         </thead>
         <tbody>
             <tr :key="user.id" v-for="user in userlist"
                 class="border-black bg-gray-100 border border-collapse"
             >
-                <td class="p-3 text-center border-r border-black">{{ user.username }}</td>
-                <td class="p-3 text-center">{{ user.email }}</td>
+                <td class="p-3 text-gray-700 text-center border-r border-black">{{ user.username }}</td>
+                <td class="p-3 text-gray-700 text-center">{{ user.email }}</td>
             </tr>
         </tbody>
     </table>
